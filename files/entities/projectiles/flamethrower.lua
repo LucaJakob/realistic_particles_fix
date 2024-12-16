4925799235f376f95d84ee3bcabc4a8d07af087e
LucaJakob <luca.jakob@stud.fhgr.ch>
2024-12-16 19:41:21 +0100

    amend flamethrower projectile

--- files/entities/projectiles/flamethrower.lua
+++ files/entities/projectiles/flamethrower.lua
@@ -28,3 +28,28 @@ for content in nxml.edit_file("data/entities/projectiles/deck/flamethrower.xml",
         end
     end
 end
+
+for content in nxml.edit_file("data/entities/projectiles/flamethrower.xml", ModTextFileGetContent, ModTextFileSetContent) do
+    local explosion = content:first_of("ProjectileComponent"):first_of("config_explosion")
+    if explosion then
+        explosion:set("sparks_count_min", "60") -- from "10"
+        :set("sparks_count_max", "80") -- from "20"
+        :set("light_fade_time", "2.8") -- from "0.8"
+    end
+
+    local emitters = content:all_of("ParticleEmitterComponent")
+
+    for i, emitter in pairs(emitters) do
+        local material = emitter:get("emitted_material_name")
+        local is_first_spark = material == "spark" and emitter:get("lifetime_min") == "0.63"
+
+        if material == "smoke" then
+            emitter:set("count_min", "6") -- from "2"
+            :set("count_max", "8") -- from "3"
+        elseif is_first_spark then
+            emitter:set("count_min", "11") -- from "1"
+            :set("count_max", "15") -- from "1"
+            :set("lifetime_max", "6.9") -- from "0.3"
+        end
+    end
+end
