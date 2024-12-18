6f1dad9d0ecc90eb58aa1c84f0773428e19b9f02
Lamia <xaxnas@gmail.com>
2024-12-18 22:38:56 +0000

    settings rewrite

--- init.lua
+++ init.lua
@@ -2,13 +2,11 @@ dofile_once("mods/realistic_particles_fix/settings.lua")
 
 local ns = "mods/" .. mod_id .. "/files/"
 
-for i, item in pairs(RP_projectiles) do
-    if not IsDisabled( item ) then
-        dofile(ns .. "entities/projectiles/" .. item.id .. ".lua")
-    end
-end
-for i, item in pairs(RP_props) do
-    if not IsDisabled( item ) then
-        dofile(ns .. "entities/props/" .. item.id .. ".lua")
+for _, category in ipairs(RP_categories) do
+    for _, item in ipairs(category) do
+        local mod_setting_id = ToSettingId(item)
+        if not ModSettingGet(mod_setting_id) then
+            dofile_once(ns .. "entities/projectiles/" .. item.id .. ".lua")
+        end
     end
 end
