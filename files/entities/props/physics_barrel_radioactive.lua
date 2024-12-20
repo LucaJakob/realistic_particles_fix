3154d9e7f66080cebb1c3a402d0ee7d60bbae288
LucaJakob <luca.jakob@stud.fhgr.ch>
2024-12-20 15:55:56 +0100

    fix rad barrel color (again)

--- files/entities/props/physics_barrel_radioactive.lua
+++ files/entities/props/physics_barrel_radioactive.lua
@@ -7,7 +7,8 @@ for content in nxml.edit_file("data/entities/props/physics_barrel_radioactive.xm
         explosion:set("explosion_sprite_lifetime", "15") -- from "10"
         :set("sparks_count_min", "1050") -- from "7"
         :set("sparks_count_max", "2050") -- from "20"
-        :set("spark_material", "grass_darker")
+        -- :set("spark_material", "grass_darker")
+        :set("spark_material", "radioactive_liquid_fading")
         :set("fade_based_on_lifetime", "1")
     end
 end
