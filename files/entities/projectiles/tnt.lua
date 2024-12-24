da339feb0441bab45ca1b722efd6ce8b389aac1f
LucaJakob <luca.jakob@stud.fhgr.ch>
2024-12-24 08:03:40 +0100

    make tnt particles not so stiff

--- files/entities/projectiles/tnt.lua
+++ files/entities/projectiles/tnt.lua
@@ -6,9 +6,13 @@ for content in nxml.edit_file("data/entities/projectiles/deck/tnt.xml", ModTextF
 
     -- Smoke
     if spark then
-        spark:set("count_min", "8") -- from "0"
-        :set("count_max", "20") -- from "4"
-        :set("lifetime_min", "20") -- from "0.1"
-        :set("lifetime_max", "30") -- from "0.3"
+        spark:set("count_min", "4") -- from "0"
+        :set("count_max", "8") -- from "4"
+        :set("lifetime_min", "1.5") -- from "0.1"
+        :set("lifetime_max", "2.5") -- from "0.3"
+        :set("airflow_force", "2.5") -- from unset
+        :set("airflow_time", "1.0") -- from unset
+        :set("airflow_scale", "0.05") -- from unset
+        :set("gravity.y", "-100")
     end
 end
