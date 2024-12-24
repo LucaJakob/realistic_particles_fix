8a9861a1671d6d4b3fd39ffc3d20069710df286b
LucaJakob <luca.jakob@stud.fhgr.ch>
2024-12-24 07:59:32 +0100

    fix tnt using real sparks and rocket

--- files/entities/projectiles/rocket_tier_3.lua
+++ files/entities/projectiles/rocket_tier_3.lua
@@ -8,23 +8,23 @@ for content in nxml.edit_file("data/entities/projectiles/deck/rocket_tier_3.xml"
         :set("sparks_count_max", "70") -- from "40"
     end
     
-    local smoke_emitter,
-        spark_emitter1,
-        spark_emitter2,
-        spark_emitter3,
-        spark_emitter4,
-        very_sparse_emitter1,
-        very_sparse_emitter2 = unpack(content:all_of("ParticleEmitterComponent"))
+    local smoke_real,
+        trail1,
+        trail2,
+        trail3,
+        trail4,
+        very_sparse1,
+        very_sparse1 = unpack(content:all_of("ParticleEmitterComponent"))
 
     -- Green Spark
-    if spark_emitter1 then
-        spark_emitter1:set("lifetime_min", "2.3") -- from "0.3"
+    if trail1 then
+        trail1:set("lifetime_min", "2.3") -- from "0.3"
         :set("lifetime_max", "5.5") -- from "0.5"
         :set("airflow_force", "1.6") -- from "0.6"
     end
 
-    if spark_emitter2 then
-        spark_emitter2:set("count_min", "15") -- from "1"
+    if trail2 then
+        trail2:set("count_min", "15") -- from "1"
         :set("count_max", "60") -- from "8"
         :set("airflow_force", "1.6") -- from "0.6"
     end
